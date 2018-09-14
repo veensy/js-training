c1be22a15ae51356d6759e53c663b60c6fe6a5a8
veensy <veensy@gmail.com>
2018-09-14 19:11:29 +0200

    is positive

--- exercises/is-positive.js
+++ exercises/is-positive.js
@@ -8,7 +8,13 @@
  */
 
 // Your code:
-
+function isPositive(number) {
+    if (number > 0){
+        return true
+    }else{
+        return false
+    }
+}
 
 //* Begin of tests
 const assert = require('assert')
